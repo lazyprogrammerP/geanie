92c8a0068a83fed80d1687ed4b5ba09752e85233
Yash <yash@newron.com>
2024-04-24 18:24:44 +0530

    Added bigquery connector

--- encoder/utils/connector/bigquery_connector.py
+++ encoder/utils/connector/bigquery_connector.py
@@ -23,39 +23,30 @@ app.add_middleware(
 
 @app.post("/big_query_injection")
 async def insert_data_into_bigquery(dataset_id: str = Form(...), table_id: str = Form(...), csv_file: UploadFile = File(...)):
-    # Initialize BigQuery client
     client = bigquery.Client()
 
-    # Read the CSV file contents    
     csv_content = await csv_file.read()
     
-    # Convert CSV content to StringIO object
     csv_stringio = StringIO(csv_content.decode('utf-8'))
     
-    # Create BigQuery dataset reference
     dataset_ref = client.dataset(dataset_id)
     
-    # Create BigQuery table reference
     table_ref = dataset_ref.table(table_id)
     
-    # Check if the table exists
     try:
         table = client.get_table(table_ref)
     except Exception as e:
-        # If the table doesn't exist, create it
         schema = []
-        # Infer schema from the first row of CSV
+        
         csv_reader = csv.reader(csv_stringio)
         header_row = next(csv_reader)
         for column_name in header_row:
             schema.append(bigquery.SchemaField(column_name, 'STRING'))
         
-        # Create the table with inferred schema
         table = bigquery.Table(table_ref, schema=schema)
         table = client.create_table(table)
 
-    # Insert data into the table
-    csv_stringio.seek(0)  # Reset StringIO object to start
+    csv_stringio.seek(0)
     data_to_insert = csv.DictReader(csv_stringio)
     errors = client.insert_rows(table, data_to_insert)
     if errors:
