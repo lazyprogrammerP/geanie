b488d37c5aa293d64c31a5360140ffc69aa7ca9a
Parshva Runwal <runwalparshva3@gmail.com>
2024-04-25 06:12:19 +0530

    chore: moved out the service URL to env

--- client/lib/service.ts
+++ client/lib/service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const service = axios.create({
-  baseURL: "http://127.0.0.1:8080",
+  baseURL: process.env.NEXT_PUBLIC_SERVICE_URL,
 });
 
 service.interceptors.request.use((config) => {
