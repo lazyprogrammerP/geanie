790f81460cc47f8febc5ddb5815c57de11db5396
Parshva Runwal <runwalparshva3@gmail.com>
2024-04-25 06:23:40 +0530

    fix: regret

--- client/lib/service.ts
+++ client/lib/service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const service = axios.create({
-  baseURL: process.env.SERVICE_URL,
+  baseURL: process.env.NEXT_PUBLIC_SERVICE_URL,
 });
 
 service.interceptors.request.use((config) => {
